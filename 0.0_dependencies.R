1b7abe4e940483cabdac005f24f8e08d543833a6
Isabella Badini <79649706+isabellabadini@users.noreply.github.com>
2024-06-25 16:22:38 +0100

    Update 0.0_dependencies.R

--- 0.0_dependencies.R
+++ 0.0_dependencies.R
@@ -2,9 +2,7 @@
 install.packages("bin/zoo_1.8-12.tar", repos = NULL, type="source")
 install.packages("bin/Rcpp_1.0.12.tar", repos = NULL, type="source")
 install.packages("bin/data.table_1.14.10.tar", repos = NULL, type="source")
-#install.packages("bin/lmtest_0.9-40.tar", repos = NULL, type="source")
 install.packages("bin/matrixStats_1.2.0.tar", repos = NULL, type="source")
 install.packages("bin/plyr_1.8.9.tar", repos = NULL, type="source")
-#install.packages("bin/sandwich_3.1-0.tar", repos = NULL, type="source")
 install.packages("bin/dplyr_1.1.4.tar.gz", repos = NULL, type="source")
 
