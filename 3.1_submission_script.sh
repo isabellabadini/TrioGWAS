3ea9215c94fa72f673fdd277846bde257de62ef5
Isabella Badini <79649706+isabellabadini@users.noreply.github.com>
2024-10-20 10:59:13 +0100

    Add sex-specific analysis scripts
    Added scripts for sex-specific analyses: 1) 3.1_unified_regression_ldak_sex.sh and 2) 3.1_submission_script.sh
    
    Scripts can be used for analyses on phenotypes including age at menarche, pubertal timing in males, and sex. Please make sure to update your config file as shown in the pipeline and add your data's variable names to the list of sex-specific phenotypes (line 35).

---
+++ 3.1_submission_script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Job name (this is what the cluster will show as running - keep short):
+#SBATCH --job-name=trio_gwas
+#
+#Project:
+#SBATCH --account=p471
+#
+#Wall clock limit (change based on resources required):
+#SBATCH --time=50:00:00 
+#
+#Output filename customization
+#This specification is Jobname_User_JobID
+#SBATCH --output=./output/%x_%u_%j.out
+#SBATCH --error=./error/trio_gwas_%j.txt
+#
+#Number of CPUs per task 
+#SBATCH --cpus-per-task=4 #amend according to the recources available to you 
+#
+# Max memory usage (change based on resources required):
+#SBATCH --mem-per-cpu=16G
+
+pheno=${1}
+
+#Working directory
+outm="/cluster/projects/trio_gwas"
+
+#Load R
+module add R/4.2.1-foss-2022a
+
+#Call script - will pass through the phenotype name in each submission
+
+$outm/3.1_unified_regression_ldak_sex.sh $pheno
